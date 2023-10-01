6ab11d28c50287f3067f561718a7f672364afb2b
rondrae <rondrae@gmail.com>
2023-10-01 15:05:50 +0000

    #31 upload index.html only when content version is changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -33,6 +33,11 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
   content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
+
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -42,6 +47,10 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
 
   etag = filemd5(var.error_html_filepath)
+
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -65,4 +74,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version  
 }
